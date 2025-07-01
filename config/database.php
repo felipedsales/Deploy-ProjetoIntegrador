07547da54e73bcb877f7d55b0de8ff219e6a6d77
Felipe de Sales <71357928+felipedsales@users.noreply.github.com>
2025-07-01 07:31:53 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'host' => getenv['DB_HOST'] ?? 'localhost',
-    'username' => getenv['DB_USERNAME'] ?? 'root',
-    'password' => getenv['DB_PASSWORD'] ?? '',
-    'database' => getenv['DB_DATABASE'] ?? 'ferraz_conecta',
+    'host' => getenv('DB_HOST') ?? 'localhost',
+    'username' => getenv('DB_USERNAME') ?? 'root',
+    'password' => getenv('DB_PASSWORD') ?? '',
+    'database' => getenv('DB_DATABASE') ?? 'ferraz_conecta',
     'charset' => 'utf8mb4',
-    'port' => getenv['DB_PORT'] ?? 3306,
-    'type' => getenv['DB_TYPE'] ?? 'mysql'
+    'port' => getenv('DB_PORT') ?? 3306,
+    'type' => getenv('DB_TYPE') ?? 'mysql'
 ]; 
